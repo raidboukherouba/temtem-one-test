cb41cf093a05841f815c8a9aadcde0e4dc4b5000
Boukherouba <101669644+Boukherouba@users.noreply.github.com>
2025-08-12 22:41:36 +0100

    Initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    ProductsModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  
+})
+export class AppModule {}
